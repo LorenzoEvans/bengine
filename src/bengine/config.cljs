f2429b1f4e79f2da6caa44f376fafdcf0c67009e
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-24 20:37:31 -0500

    many tweaks

--- src/bengine/config.cljs
+++ src/bengine/config.cljs
@@ -1,12 +1,12 @@
 (ns bengine.config
-  (:require [macchiato.env :as config]
+  (:require [macchiato.env :as configr]
             [mount.core :refer [defstate]]))
 
 (defstate env :start
   (merge
    {:posts-dir "_posts"
     :out-dir "public"}
-   (config/env)))
+   (configr/env)))
 
 (defn posts-dir [] (:posts-dir @env))
 (defn out-dir [] (:out-dir @env))
